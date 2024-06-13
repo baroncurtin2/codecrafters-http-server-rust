aabfc28d00c4a0e227936a1dd3f1c39db20de1d3
baroncurtin2 <knowledge3@gmail.com>
2024-06-12 22:30:20 -0400

    return a file

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,17 @@
+use itertools::Itertools;
 use std::{
-    env, fs,
-    io::{self, Read, Write},
-    net::{TcpListener, TcpStream},
+    env,
+    fs::File,
+    io::{self, BufRead, BufReader, Read, Write},
+    net::TcpListener,
+    path::Path,
     thread,
 };
 
-const OK_RESPONSE: &str = "HTTP/1.1 200 OK\r\n\r\n";
-const NOT_FOUND_RESPONSE: &str = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
-const METHOD_NOT_ALLOWED_RESPONSE: &str = "HTTP/1.1 405 Method Not Allowed\r\n\r\n";
-const CONTENT_TYPE_PLAIN_TEXT: &str = "Content-Type: text/plain\r\n";
-const CONTENT_TYPE_OCTET_STREAM: &str = "Content-Type: application/octet-stream\r\n";
-
-#[derive(Debug)]
-struct HttpRequest {
-    method: String,
-    path: String,
-    host: Option<String>,
-    user_agent: Option<String>,
-}
-
-impl HttpRequest {
-    fn new(method: String, path: String) -> Self {
-        HttpRequest {
-            method,
-            path,
-            host: None,
-            user_agent: None,
-        }
-    }
-
-    fn with_host(mut self, host: String) -> Self {
-        self.host = Some(host);
-        self
-    }
-
-    fn with_user_agent(mut self, user_agent: String) -> Self {
-        self.user_agent = Some(user_agent);
-        self
-    }
-}
-
 fn main() -> io::Result<()> {
+    println!("Logs from your program will appear here!");
+
     let listener = TcpListener::bind("127.0.0.1:4221")?;
-    println!("Server listening on http://127.0.0.1:4221");
 
     for stream in listener.incoming() {
         match stream {
@@ -62,105 +31,79 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-fn handle_connection(mut stream: TcpStream) -> io::Result<()> {
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer)?;
-    let req_str = String::from_utf8_lossy(&buffer[..]);
-    let http_request = parse_request(&req_str)?;
+fn handle_connection(stream: std::net::TcpStream) -> io::Result<()> {
+    println!("Accepted new connection");
 
-    let response = match http_request.method.as_str() {
-        "GET" => handle_get_request(&http_request),
-        _ => METHOD_NOT_ALLOWED_RESPONSE.to_string(),
-    };
+    let mut reader = BufReader::new(&stream);
+    let mut req = String::new();
+    reader.read_line(&mut req)?;
 
-    stream.write_all(response.as_bytes())?;
+    if let Some(path) = req.split_whitespace().nth(1) {
+        match path {
+            "/user-agent" => handle_user_agent(&mut reader, &stream)?,
+            "/echo" => handle_echo(&req, &stream)?,
+            p if p.starts_with("/files/") => handle_file_request(p, &stream)?,
+            "/" => stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n")?,
+            _ => stream.write_all(b"HTTP/1.1 404 Not Found\r\n\r\n")?,
+        }
+    }
+
     Ok(())
 }
 
-fn parse_request(request: &str) -> io::Result<HttpRequest> {
-    let mut lines = request.lines();
-    let first_line = lines
-        .next()
-        .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidInput, "Empty request"))?;
+fn handle_user_agent(reader: &mut BufReader<&TcpStream>, stream: &TcpStream) -> io::Result<()> {
+    let user_agent = reader
+        .lines()
+        .find_map(|line| {
+            line.ok()
+                .and_then(|l| l.strip_prefix("User-Agent: ").map(|s| s.to_string()))
+        })
+        .ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, "User-Agent header not found"))?;
 
-    let mut parts = first_line.split_whitespace();
-    let method = parts.next().unwrap_or_default().to_string();
-    let path = parts.next().unwrap_or_default().to_string();
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+        user_agent.len(),
+        user_agent
+    );
+    stream.write_all(response.as_bytes())?;
 
-    let (host, user_agent) = lines.fold((None, None), |acc, line| {
-        if line.starts_with("Host: ") {
-            (Some(line.replace("Host: ", "").trim().to_string()), acc.1)
-        } else if line.starts_with("User-Agent: ") {
-            (
-                acc.0,
-                Some(line.replace("User-Agent: ", "").trim().to_string()),
-            )
-        } else {
-            acc
-        }
-    });
-
-    Ok(HttpRequest::new(method, path)
-        .with_host(host.unwrap_or_default())
-        .with_user_agent(user_agent.unwrap_or_default()))
+    Ok(())
 }
 
-fn handle_get_request(http_request: &HttpRequest) -> String {
-    match http_request.path.as_str() {
-        "/" => OK_RESPONSE.to_string(),
-        path if path.starts_with("/echo/") => handle_echo_request(path),
-        "/user-agent" => handle_user_agent_request(http_request),
-        path if path.starts_with("/files/") => handle_file_request(http_request),
-        _ => NOT_FOUND_RESPONSE.to_string(),
-    }
+fn handle_echo(req: &str, stream: &TcpStream) -> io::Result<()> {
+    let s = req.splitn(2, ' ').nth(1).unwrap_or("");
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+        s.len(),
+        s
+    );
+    stream.write_all(response.as_bytes())?;
+
+    Ok(())
 }
 
-fn handle_echo_request(path: &str) -> String {
-    let body = path.replace("/echo/", "");
-    format!(
-        "{}{}Content-Length: {}\r\n\r\n{}",
-        OK_RESPONSE,
-        CONTENT_TYPE_PLAIN_TEXT,
-        body.len(),
-        body
-    )
-}
+fn handle_file_request(path: &str, stream: &TcpStream) -> io::Result<()> {
+    let file_path = path.trim_start_matches("/files/");
+    let directory = env::args()
+        .nth(2)
+        .ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, "Directory argument missing"))?;
+    let full_path = Path::new(&directory).join(file_path);
 
-fn handle_user_agent_request(http_request: &HttpRequest) -> String {
-    if let Some(user_agent) = &http_request.user_agent {
-        format!(
-            "{}{}Content-Length: {}\r\n\r\n{}",
-            OK_RESPONSE,
-            CONTENT_TYPE_PLAIN_TEXT,
-            user_agent.len(),
-            user_agent
-        )
-    } else {
-        NOT_FOUND_RESPONSE.to_string()
-    }
-}
+    let mut file = File::open(&full_path).map_err(|e| {
+        eprintln!("Error opening file {}: {}", full_path.display(), e);
+        io::Error::new(io::ErrorKind::NotFound, "File not found")
+    })?;
 
-fn handle_file_request(http_request: &HttpRequest) -> String {
-    let file_name = http_request.path.replace("/files/", "");
-    let mut dir = match env::args().nth(1) {
-        Some(dir) => dir,
-        None => {
-            eprintln!("Error: --directory argument not provided.");
-            return NOT_FOUND_RESPONSE.to_string();
-        }
-    };
+    let metadata = file.metadata()?;
+    let mut buffer = vec![0; metadata.len() as usize];
+    file.read_exact(&mut buffer)?;
 
-    dir.push_str(&file_name);
-    match fs::read(dir) {
-        Ok(contents) => {
-            format!(
-                "{}{}Content-Length: {}\r\n\r\n{}",
-                OK_RESPONSE,
-                CONTENT_TYPE_OCTET_STREAM,
-                contents.len(),
-                String::from_utf8_lossy(&contents)
-            )
-        }
-        Err(_) => NOT_FOUND_RESPONSE.to_string(),
-    }
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {}\r\n\r\n",
+        buffer.len()
+    );
+    stream.write_all(response.as_bytes())?;
+    stream.write_all(&buffer)?;
+
+    Ok(())
 }
