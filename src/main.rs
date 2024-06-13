04525986d780c063f375c7923ed4373eda61d736
baroncurtin2 <knowledge3@gmail.com>
2024-06-12 20:21:07 -0400

    respond with 200

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{
+    io::Write,
+    net::{TcpListener, TcpStream},
+    thread,
+};
+
+use nom::AsBytes;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,13 +14,17 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        let stream = stream?;
+        thread::spawn(|| {
+            handle_connection(stream);
+        });
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    // write the response to the stream
+    stream.write_all(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
